57f8290cfd70cc0574ed2018728dc0639b5b7e2a
Stefan2834 <111298447+Stefan2834@users.noreply.github.com>
2023-01-14 16:49:57 +0200

    main

--- index.js
+++ index.js
@@ -327,3 +327,65 @@ function bataliiOpacity () {
         ypres.style.zIndex = '10';
     }
 }
+
+
+var options = {
+    series: [{
+    data: [850000, 700000, 500000, 500000, 400000]
+  }],
+    chart: {
+    width: '100%',
+    type: 'bar',
+  },
+  colors:['#f06529', '#2965f1', '#f7df1e', '#0769ad', '#fecd69'] ,
+  plotOptions: {
+    bar: {
+      columnWidth: '45%',
+      distributed: true,
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+        return val;
+      }
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: [
+      ['Html5'],
+      ['Css5'],
+      ['JavaScript'],
+      ['Jquery'],
+      ['OtherJs', 'Libraries'],
+    ],
+    labels: {
+      style: {
+        fontSize: '13px',
+        colors:'white',
+        fontFamily:'Poppins',
+        }
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val;
+      }
+    }
+  
+  },
+}
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
