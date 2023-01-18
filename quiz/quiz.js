98b5f0c416fd01d2344c1ebae01b5b18da613272
Stefan2834 <111298447+Stefan2834@users.noreply.github.com>
2023-01-18 17:35:56 +0200

    main

---
+++ quiz/quiz.js
@@ -0,0 +1,20 @@
+let value = document.querySelectorAll(".value");
+let submit = document.getElementById("submit");
+let score = 0;
+
+submit.addEventListener("submit", function (e) {
+    e.preventDefault()
+    value.forEach(value => {
+        if(value.checked) {
+            if(value.value === 'true') {
+                score += 1;
+            } else {
+                value.style.color = 'black';
+            }
+            value.checked = false;
+        }
+    })
+    document.body.innerHTML = `<div class="score"><div>Scorul tau: ${score}<div><a href='/'>Inapoi</a></div>`
+    score = 0;
+})
+
