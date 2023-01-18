b68b4c2717bf8abc62c0f88691f5ebe96e9410a8
Stefan2834 <111298447+Stefan2834@users.noreply.github.com>
2023-01-18 17:44:52 +0200

    main

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -14,7 +14,7 @@ submit.addEventListener("submit", function (e) {
             value.checked = false;
         }
     })
-    document.body.innerHTML = `<div class="score"><div>Scorul tau: ${score}<div><a href='/'>Inapoi</a></div>`
+    document.body.innerHTML = `<div class="score"><div>Punctajul tău: ${score}<div><a href='/'>Inapoi</a></div>`
     score = 0;
 })
 
